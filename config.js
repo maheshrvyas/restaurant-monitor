a6dcf7f081d20c6fdb45dbc2abda41a128dd3c17
maheshrvyas <6512961+maheshrvyas@users.noreply.github.com>
2025-10-04 23:41:27 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+exports.restaurants = [
+  {
+    name: "Swiggy - Domino's Banjara Hills",
+    url: "https://www.swiggy.com/restaurants/dominos-banjara-hills-hyderabad-12345"
+  },
+  {
+    name: "Swiggy - Domino's Hitech City",
+    url: "https://www.swiggy.com/restaurants/dominos-hitech-city-hyderabad-67890"
+  },
+  // Add 3 more listings here
+];
